5d2f129f63d83fbe4786786a57b697a4a3fe8591
unknown <divyacharaninabha@gmail.com>
2020-09-25 09:56:04 +0530

    Added: Welcome message added

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -4,6 +4,11 @@ public class EmployeeWageProblem {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//Welcome message
+		System.out.println("Welcome to Employee Wage Computation Program");
+		
+		
 
 	}
 
