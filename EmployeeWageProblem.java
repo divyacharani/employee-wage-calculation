cbca3f854584e11c360539a2a47f1be68b838de2
unknown <divyacharaninabha@gmail.com>
2020-09-25 10:00:57 +0530

    Added: Employee attendance check is added

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -1,12 +1,23 @@
 package com.bridgelabz.employeeWages;
 
 public class EmployeeWageProblem {
+	//Constants
+	public static final int IS_PRESENT = 1;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		//Welcome message
 		System.out.println("Welcome to Employee Wage Computation Program");
+		
+		int empCheck = (int)(Math.floor(Math.random()*10) % 2);
+		
+		if(empCheck==IS_PRESENT) {
+			System.out.println("Employee Present");
+		}
+		else {
+			System.out.println("Employee Absent");
+		}
 		
 		
 
