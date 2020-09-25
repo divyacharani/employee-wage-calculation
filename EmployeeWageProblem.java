4376b9b18565dddac8efbde24a6669c98fa3b526
unknown <divyacharaninabha@gmail.com>
2020-09-25 10:06:44 +0530

    Added: Employee wage calclation is added

--- EmployeeWageProblem.java
+++ EmployeeWageProblem.java
@@ -2,25 +2,38 @@ package com.bridgelabz.employeeWages;
 
 public class EmployeeWageProblem {
 	//Constants
-	public static final int IS_PRESENT = 1;
+		public static final int IS_PRESENT = 1;
+		public static final int EMP_RATE_PER_HOUR = 20;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		//Welcome message
-		System.out.println("Welcome to Employee Wage Computation Program");
-		
-		int empCheck = (int)(Math.floor(Math.random()*10) % 2);
-		
-		if(empCheck==IS_PRESENT) {
-			System.out.println("Employee Present");
-		}
-		else {
-			System.out.println("Employee Absent");
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			//Variable
+			int empWage = 0;
+			int empHours = 0;
+			
+			System.out.println("Welcome to Employee Wage Computation Program");
+			
+			//Computation
+			int empCheck = (int)(Math.floor(Math.random()*10) % 2);
+			
+			if(empCheck==IS_PRESENT) {
+				empHours = 8;
+				System.out.println("Employee Present");
+			}
+			else {
+				empHours = 0;
+				System.out.println("Employee Absent");
+			}
+			empWage = empHours * EMP_RATE_PER_HOUR;
+			System.out.println("Wage : "+empWage); 		
+			
+			
+
 		}
 		
 		
 
-	}
+	
 
 }
