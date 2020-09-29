1bf55b60f8557c5e423dc9f4d47425b6ed57dac4
unknown <divyacharaninabha@gmail.com>
2020-09-29 10:35:55 +0530

    Added: Interface added

---
+++ EmployeeWages.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.employeeWages;
+
+public interface EmployeeWages {
+
+	////Method to add company object to companyArray
+	public void addCompany(String companyName, int empRatePerHour, int workingDays, int hoursLimit);
+	public void wageComputation();
+	//Method to Compute of employee wages
+	public void computeWage(Company object);
+}
