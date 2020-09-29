f958e1ef35129efba8ad93b7506ad29d20c2c5f8
unknown <divyacharaninabha@gmail.com>
2020-09-29 10:16:51 +0530

    Added: Multiple company objects were added

--- Company.java
+++ Company.java
@@ -1,17 +1,12 @@
  package com.bridgelabz.employeeWages;
 
 public class Company {
-	//Constants
-	public static final int IS_FULL_TIME = 1;
-	public static final int IS_PART_TIME = 2;
-	
 	//Attributes
 	private String companyName;
 	private int empRatePerHour;
 	private int workingDays;
 	private int hoursLimit;
 	private int totalWage;
-	private String empType;
 	
 	//Constructor
 	public Company(String companyName, int empRatePerHour, int workingDays, int hoursLimit) {
@@ -20,36 +15,37 @@ public class Company {
 		this.workingDays = workingDays;
 		this.hoursLimit = hoursLimit;
 	}
-	
-	//Computation of employee wages
-	public void computeWage() {
-		//Variables
-		int empHours=0, noOfHour=0, noOfDays=0;
-		System.out.println("Daily wages for "+companyName+" is");
-		System.out.println("Day\tEmployee Hour\tEmployee Type");
-		while(noOfHour<=hoursLimit && noOfDays<=workingDays) {
-			int empCheck = (int)(Math.floor(Math.random()*10) % 3);
-			noOfDays++;
-			//Check whether employee is part time, full time or absent
-			switch(empCheck) {
-			case IS_PART_TIME :
-				empType = "Part Time and present";
-				empHours = 4;
-				break;
-			case IS_FULL_TIME :
-				empType = "Full Time and present";
-				empHours = 8;
-				break;
-			default:
-				empType = "Employee absent";
-				empHours = 0;	
-			}
-			noOfHour += empHours;
-			System.out.println(noOfDays+"\t"+empHours+"\t"+empType);
-		}
-		totalWage = noOfHour*empRatePerHour;
-		
+	public String getCompanyName() {
+		return companyName;
 	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public int getEmpRatePerHour() {
+		return empRatePerHour;
+	}
+	public void setEmpRatePerHour(int empRatePerHour) {
+		this.empRatePerHour = empRatePerHour;
+	}
+	public int getWorkingDays() {
+		return workingDays;
+	}
+	public void setWorkingDays(int workingDays) {
+		this.workingDays = workingDays;
+	}
+	public int getHoursLimit() {
+		return hoursLimit;
+	}
+	public void setHoursLimit(int hoursLimit) {
+		this.hoursLimit = hoursLimit;
+	}
+	public int getTotalWage() {
+		return totalWage;
+	}
+	public void setTotalWage(int totalWage) {
+		this.totalWage = totalWage;
+	}
+
 	public String toString() {
 		return "Total Employee wage for Company "+companyName+" is "+totalWage;
 	}
